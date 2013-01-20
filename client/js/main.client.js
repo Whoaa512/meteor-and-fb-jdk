c341e69d11368bf603527d1fa9cb95c562892c34
Whoaa512 <whoaa512@gmail.com>
2013-01-20 11:35:55 -0800

    Added client/server code to display user's facebook pic

--- client/js/main.client.js
+++ client/js/main.client.js
@@ -4,4 +4,13 @@
 Meteor.subscribe('fbAccessToken');
 
 Meteor.startup(function () {
+
+  // helper function to display the pic on the page
+  Template.fb_pic.pic = function() {
+    var userProfile;
+    userProfile = Meteor.user().profile;
+    if (userProfile) {
+      return userProfile.picture;
+    }
+  };
 });
--- server/main.server.js
+++ server/main.server.js
@@ -11,4 +11,31 @@ Meteor.startup(function () {
     });
   });
 
+  // during new account creation get user picture from facebook and save it on user object
+  Accounts.onCreateUser(function(options, user) {
+    if (options.profile) {
+      options.profile.picture = getFbPicture(user.services.facebook.accessToken);
+      // We still want the default 'profile' behavior.
+      user.profile = options.profile;
+    }
+    return user;
+  });
+
+  // make async call to grab the picture from facebook
+  var getFbPicture = function(accessToken) {
+    var result;
+    result = Meteor.http.get("https://graph.facebook.com/me", {
+      params: {
+        access_token: accessToken,
+        fields: 'picture'
+      }
+    });
+    if (result.error) {
+      throw result.error;
+    }
+
+    // save the picture's url instead of the picture object we are given from facebook
+    return result.data.picture.data.url;
+  };
+
 });
